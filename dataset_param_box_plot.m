85b9386dbd55bfae19826c650af65e4dae4aabff
Nick Tolley <nic.m.tolley@gmail.com>
2022-09-12 21:01:28 -0400

    Add files for box plot and selectivity figs

---
+++ dataset_param_box_plot.m
@@ -0,0 +1,189 @@
+clear all;
+close all;
+
+load('all_param_data.mat')
+
+muscle_list = {'Carpi_Radialis', 'Bicep', 'Deltoid', 'Tricep', 'Wrist_Extensor', 'Acromiotrapezius'};
+
+muscle_names = {'W. Flexor','Biceps','Deltoid','Triceps','W. Extensor','Acromiotrapezius'};
+
+
+%% Filter Sigmoid Structure
+% sigmoid_struct = sigmoid_struct(["transq_rat_8"] ~= [sigmoid_struct.Rat]);
+sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Position}, 'C4') == 0);
+sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Position}, 'C6') == 0);
+
+sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Position}, 'C5'));
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Depth}, 'Epidural'));
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Depth}, 'Spinous_Process'));
+
+%Remove 1ms monophasic pulse
+stim_filter = strcmp({sigmoid_struct.Duration}, '1_ms') + strcmp({sigmoid_struct.Stim_Waveform},'Monophasic')+...
+    strcmp({sigmoid_struct.Stim_Freq}, 'Single') ~= 3;
+
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Duration}, '1_ms'));
+sigmoid_struct = sigmoid_struct(stim_filter);
+
+
+
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Stim_Freq}, '10_kHz'));
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Stim_Freq}, 'Single'));
+sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Stim_Waveform}, 'Monophasic'));
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Stim_Waveform}, 'Biphasic'));
+sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Lateral_Position}, 'Midline'));
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Lateral_Position}, 'Lateral'));
+
+filtered_struct = sigmoid_struct;
+
+%Find fields with no missing entries
+filled_muscle = {};
+for muscle = 1:length(muscle_list)
+    muscle_param_matrix = [];
+    empty_field = 0;
+    for stim_set = 1:length(filtered_struct)
+        if isempty(filtered_struct(stim_set).(muscle_list{muscle})) == 1
+            empty_field = empty_field + 1;
+        end
+    end
+    
+    if empty_field == 0
+        filled_muscle{end+1} = muscle_list{muscle};
+    end
+end
+
+
+%% Organize for statistics 
+% Choose parameter to plot (1 = stim threshold, 2 = asymptote,
+% 3 = midpoint, 4 = slope, 5 = max_activation, 6 = max_charge)
+sig_param = 1;
+
+figure
+%Plot by depth
+d_vec = {[],[]};
+color_map = {[1, 0, 0],[0,0,0]};
+bar_color_map = {[1, 0, 0],[0,0,0]+0.4};
+
+%single pulse array
+distrib1_array = {d_vec,d_vec,d_vec,d_vec,d_vec,d_vec};
+%10 kHz arrary
+distrib2_array = {d_vec,d_vec,d_vec,d_vec,d_vec,d_vec};
+
+for stim_set = 1:length(sigmoid_struct)
+    for muscle = 1:length(muscle_list)
+        plot_data = sigmoid_struct(stim_set).(muscle_list{muscle});
+        if isempty(plot_data) ~= 1
+            plot_data = plot_data(sig_param);
+        
+            if strcmp(sigmoid_struct(stim_set).Stim_Freq,'Single') == 1 && strcmp(sigmoid_struct(stim_set).Depth,'Epidural') == 1
+                distrib1_array{muscle}{1}(end+1) = plot_data;
+            elseif strcmp(sigmoid_struct(stim_set).Stim_Freq,'Single') == 1 && strcmp(sigmoid_struct(stim_set).Depth,'Spinous_Process') == 1
+                distrib1_array{muscle}{2}(end+1) = plot_data;           
+            elseif strcmp(sigmoid_struct(stim_set).Stim_Freq,'10_kHz') == 1 && strcmp(sigmoid_struct(stim_set).Depth,'Epidural') == 1
+                distrib2_array{muscle}{1}(end+1) = plot_data;
+            elseif strcmp(sigmoid_struct(stim_set).Stim_Freq,'10_kHz') == 1 && strcmp(sigmoid_struct(stim_set).Depth,'Spinous_Process') == 1
+                distrib2_array{muscle}{2}(end+1) = plot_data;
+            end           
+        end
+    end
+end
+
+
+for muscle = 1:length(muscle_list)
+    subplot(2,3,muscle)
+%     ylim manual
+    hold on
+    
+%     bar_pos = {[0.9, 1.9], [1.1, 2.1]};
+    bar_pos = {[1, 1.8], [1.2, 2]};
+
+    for idx = 1:length(d_vec)
+        distrib1 = distrib1_array{muscle}{idx};
+        distrib2 = distrib2_array{muscle}{idx};
+        
+        [f1,xi_1] = ksdensity(distrib1);
+        mean1 = nanmean(distrib1);
+        ste1 = nanstd(distrib1)/sqrt(length(distrib1));   
+        
+        [f2,xi_2] = ksdensity(distrib2);
+        mean2 = nanmean(distrib2);
+        ste2 = nanstd(distrib2)/sqrt(length(distrib2));
+        
+        plot(bar_pos{idx}, [mean1 mean2],'--' ,'color',bar_color_map{idx},'LineWidth',2)
+
+        plot(rescale((f1*-1),0,0.4) + 0.4, xi_1,'--','color',color_map{idx},'LineWidth',1.3)
+        a1 = bar(bar_pos{idx}(1), mean1,0.1, 'FaceColor',bar_color_map{idx},'LineWidth',1.3);
+        errorbar(bar_pos{idx}(1), mean1, ste1, 'k','LineWidth',1.3)    
+        
+        plot(rescale((f2),0,0.4) + 2.2, xi_2,'--','color',color_map{idx},'LineWidth',1.3)
+        a2 = bar(bar_pos{idx}(2), mean2,0.1,'FaceColor', bar_color_map{idx},'LineWidth',1.3);
+        errorbar(bar_pos{idx}(2), mean2, ste2, 'k','LineWidth',1.3)
+        
+        
+        
+    end
+    plot([0.8,0.8],[-1e4 1e4],'k','LineWidth',2)
+    plot([2.2,2.2],[-1e4 1e4],'k','LineWidth',2)
+end
+for muscle = 1:length(muscle_list)
+    subplot(2,3,muscle)
+    hold on
+    a2 = bar(-10, 10, 'FaceColor', [0 0 0]+0.4,'LineWidth',1.3);
+    a1 = bar(-10, 10, 'r','LineWidth',1.3);
+    title(muscle_names{muscle})
+    xlim([0 3])
+    ylim([0 5000])
+    xticks([0.9 2.1])
+    xticklabels({'Single', '10 kHz'})
+    ylabel('Max Current (uA)')
+    if muscle == 1
+        legend([a1 a2],{'Epidural', 'Spinous Process'}, 'Location', 'northwest')
+    end
+%     legend({'Epidural','Spinous Process'})
+    ax = gca;
+    ax.FontSize = 17;
+    
+    
+end
+
+
+% for stim_type = 1:length(stim_type_list) 
+%    boxplot_data = [];
+%    plot_data_struct = sigmoid_struct(strcmp({sigmoid_struct.stim_idx}, stim_type_list{stim_type}));
+%    for stim_set = 1:length(plot_data_struct)
+%        for muscle = 1:length(filled_muscle)
+%            boxplot_data(stim_set,muscle) = plot_data_struct(stim_set).(filled_muscle{muscle})(sig_param);
+%        end
+%    end
+%    
+%    figure
+%    hold on
+% %    grid on
+%    boxplot(boxplot_data,[1:5],'orientation', 'horizontal')
+%    
+%    for r = 1:size(boxplot_data,1)
+%        scatter(boxplot_data(r,:),1:5,'filled')
+%    end
+%    ylim([0 6])
+%    xlim([-500 3000])
+% %    xlim([0 8000])
+% %    xlim([0 max(max(boxplot_data))+500])
+% 
+%    title(stim_type_list{stim_type},'Interpreter','none')
+%    yticks(1:6)
+%    yticklabels(filled_muscle)
+%    set(gca,'TickLabelInterpreter','none')
+%    ylabel('Muscle')
+%    xlabel('Activation Threshold (uA)')
+%    
+% %    figure
+% %    notBoxPlot(box_plot_data,1:5)
+% %    title(stim_type_list{stim_type},'Interpreter','none')
+%    
+% end
+   
+
+    
+
+   
+
+    
---
+++ segment_selectivity_v3_line_hm.m
@@ -0,0 +1,237 @@
+close all;
+clear all;
+
+close all;
+clear all;
+
+load('all_param_data.mat')
+
+muscle_list = {'Carpi_Radialis', 'Bicep', 'Deltoid', 'Tricep', 'Wrist_Extensor', 'Acromiotrapezius'};
+
+muscle_names = {'W. Flexor','Biceps','Deltoid','Triceps','W. Extensor','A. Trapezius'};
+
+
+%% Filter Sigmoid Structure
+% sigmoid_struct = sigmoid_struct(["transq_rat_8"] ~= [sigmoid_struct.Rat]);
+sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Position}, 'C4') == 0);
+sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Position}, 'C6') == 0);
+
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Position}, 'C5'));
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Depth}, 'Epidural'));
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Depth}, 'Spinous_Process'));
+
+%Remove 1ms monophasic pulse
+stim_filter = strcmp({sigmoid_struct.Duration}, '1_ms') + strcmp({sigmoid_struct.Stim_Waveform},'Monophasic')+...
+    strcmp({sigmoid_struct.Stim_Freq}, 'Single') ~= 3;
+
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Duration}, '1_ms'));
+sigmoid_struct = sigmoid_struct(stim_filter);
+
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Stim_Freq}, '10_kHz'));
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Stim_Freq}, 'Single'));
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Stim_Waveform}, 'Monophasic'));
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Stim_Waveform}, 'Biphasic'));
+sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Lateral_Position}, 'Midline'));
+% sigmoid_struct = sigmoid_struct(strcmp({sigmoid_struct.Lateral_Position}, 'Lateral'));
+
+
+%% Organize for statistics 
+% Choose parameter to plot (1 = stim threshold, 2 = asymptote,
+% 3 = midpoint, 4 = slope, 5 = max_activation, 6 = max_charge)
+sig_param = 1;
+
+stim_string_array = {};
+stim_string_general = {};
+for stim_set = 1:length(sigmoid_struct)
+    stim_string = [sigmoid_struct(stim_set).Rat{1},'_',sigmoid_struct(stim_set).Stim_Freq,'_',...
+        sigmoid_struct(stim_set).Stim_Waveform,'_', sigmoid_struct(stim_set).Depth];
+    
+    stim_string_array{end+1} = stim_string;
+    
+    stim_string2 = [sigmoid_struct(stim_set).Stim_Freq,'_',...
+        sigmoid_struct(stim_set).Stim_Waveform,'_', sigmoid_struct(stim_set).Depth];
+    stim_string_general{end+1} = stim_string2;
+    
+    sigmoid_struct(stim_set).stim_idx = stim_string2;
+    
+    %Set poor fits to threshold of 8000, store inv(threshold)
+    for muscle = 1:length(muscle_list)
+       if any(isnan(sigmoid_struct(stim_set).(muscle_list{muscle}))) == 1
+           sigmoid_struct(stim_set).(muscle_list{muscle}) = inv(8000);
+       elseif isempty(sigmoid_struct(stim_set).(muscle_list{muscle})) ~= 1
+           muscle_data = sigmoid_struct(stim_set).(muscle_list{muscle});
+           sigmoid_struct(stim_set).(muscle_list{muscle}) = inv(muscle_data(1));
+           
+       end
+        
+    end
+    
+    %Compute BIC/TRI/DEL Selectivity
+    if (isempty(sigmoid_struct(stim_set).Tricep) + isempty(sigmoid_struct(stim_set).Bicep) + isempty(sigmoid_struct(stim_set).Deltoid)+ isempty(sigmoid_struct(stim_set).Carpi_Radialis)) == 0
+        BIC_ex = sigmoid_struct(stim_set).Bicep;
+        TRI_ex = sigmoid_struct(stim_set).Tricep;
+        DEL_ex = sigmoid_struct(stim_set).Deltoid;
+        CAR_ex = sigmoid_struct(stim_set).Carpi_Radialis;
+        
+        sigmoid_struct(stim_set).BIC_sel = BIC_ex/(BIC_ex+TRI_ex+DEL_ex+CAR_ex);
+        sigmoid_struct(stim_set).TRI_sel = TRI_ex/(BIC_ex+TRI_ex+DEL_ex+CAR_ex);
+        sigmoid_struct(stim_set).DEL_sel = DEL_ex/(BIC_ex+TRI_ex+DEL_ex+CAR_ex);
+        sigmoid_struct(stim_set).CAR_sel = CAR_ex/(BIC_ex+TRI_ex+DEL_ex+CAR_ex);
+        
+    end
+    
+    
+end
+
+
+
+stim_type_list = unique(stim_string_array);
+stim_type_general = unique(stim_string_general);
+
+%% Compute segmental selectivity
+% rat_list = [8,9,10,11,12,14,15,16,17];
+rat_list = [10,11,15,17];
+
+
+% field_name_list = {'10k Bi Epi','10k Bi Sp','10k Mono Epi','10k Mono Sp',...
+%     'Single Bi Epi','Single Bi Sp','Single Mono Epi','Single Mono Sp'};
+field_name_list = {'10 kHz','10 kHz','110 kHz','10 kHz',...
+    'Single Pulse','Single Pulse','Single Pulse','Single Pulse'};
+
+%{stim_type 8 cells}=>{position 3 cells}=>{rat x muscle (DEL, BIC, TRI)}
+arm_sel_data = {{[],[],[],[]},{[],[],[],[]},{[],[],[],[]},{[],[],[],[]},{[],[],[],[]},{[],[],[],[]},{[],[],[],[]},{[],[],[],[]}};
+
+wrist_sel_struct = struct();
+for rat = 1:length(rat_list)
+    for stim_type = 1:length(stim_type_general)
+        rat_filter = strcmp(['transq_rat_',num2str(rat_list(rat))],[sigmoid_struct.Rat]);
+        stim_filter = strcmp(stim_type_general{stim_type},{sigmoid_struct.stim_idx});
+               
+        C3_filter = (strcmp('C3',{sigmoid_struct.Position})+rat_filter+stim_filter) == 3;
+        C5_filter = (strcmp('C5',{sigmoid_struct.Position})+rat_filter+stim_filter) == 3;
+        C7_filter = (strcmp('C7',{sigmoid_struct.Position})+rat_filter+stim_filter) == 3;
+        
+        filter_array = {C3_filter,C5_filter,C7_filter};
+        
+        if any(C3_filter) && any(C5_filter)&& any(C7_filter)
+%             BIC_val = sigmoid_struct(C3_filter).BIC_sel;      
+%             TRI_val = sigmoid_struct(C7_filter).TRI_sel;      
+%             DEL_val = sigmoid_struct(C7_filter).DEL_sel;
+            
+           	for f = 1:length(filter_array) 
+                arm_sel_data{stim_type}{f}(rat,1) = sigmoid_struct(filter_array{f}).BIC_sel;
+                arm_sel_data{stim_type}{f}(rat,2) = sigmoid_struct(filter_array{f}).DEL_sel;
+                arm_sel_data{stim_type}{f}(rat,3) = sigmoid_struct(filter_array{f}).TRI_sel;
+                arm_sel_data{stim_type}{f}(rat,4) = sigmoid_struct(filter_array{f}).CAR_sel;
+                
+            end
+
+        end
+        
+        
+    end
+    
+end
+
+%% Plot selectivity heatmaps
+
+figure
+count = 1;
+for stim_type = [1 5]
+    stim_data = arm_sel_data{stim_type};
+ 
+    subaxis(2,2,count, 'Spacing', 0.15, 'Padding', 0, 'Margin', 0.1);
+%     hold on
+    
+    data_matrix = [];
+    ste_matrix = [];
+    
+    for pos = 1:3
+        pos_data = stim_data{pos};
+        %Eliminate missing rows
+        pos_data = pos_data(sum(pos_data,2) > 0,:);
+        avg_data = mean(pos_data,1);
+        ste_data = std(pos_data,1)/sqrt(size(pos_data,1));
+        
+        data_matrix(pos,:) = avg_data;
+        ste_matrix(pos,:) = ste_data;
+        
+        
+    end
+    
+%     xticklabels({'DEL','BIC','TRI', 'CAR'})
+%     xticks([1 2 3 4])
+%     yticklabels({'C7','C5','C3'})
+%     yticks([1 2 3])
+    
+    
+%     imagesc(flipud(data_matrix), [0.25 0.5])
+%     imagesc(flipud(ste_matrix), [0 0.05])
+    h = heatmap({'BIC','DEL','TRI', 'FCR'},{'C3','C5','C7'},data_matrix,'CellLabelColor','none');
+    h.Title = field_name_list{stim_type};
+    caxis(h, [0.15 0.35])
+    
+
+    colorbar
+    ax = gca;
+    ax.FontSize = 14;
+    count = count+1;
+end
+
+%% Line Plots
+% ***DOUBLE CHECK THIS LABELING
+muscle_title = {'Biceps','DEL','Triceps','FCR'};
+
+% stim_idx_list = [1 5]; %Epidural Bi
+% stim_idx_list = [3 7]; %Epidural Mono
+stim_idx_list = [2 6]; %Sp Bi
+% stim_idx_list = [4 8]; %Sp Mono
+
+% stim_idx_list = [2 4 6 8]; %Spinous Process set
+line_x_data = {[0.8 1.8 2.8],[],[],[] ,[1.2 2.2 3.2]};
+
+for muscle = [1 3]
+    subaxis(2,2,count, 'Spacing', 0.13, 'Padding', 0, 'Margin', 0.1);
+    hold on
+    for stim_type = 1:length(stim_idx_list)
+
+        stim_data = arm_sel_data{stim_idx_list(stim_type)};
+
+        data_matrix = [];
+        ste_matrix = [];
+
+        for pos = 1:3
+            pos_data = stim_data{pos};
+            %Eliminate missing rows
+            pos_data = pos_data(sum(pos_data,2) > 0,:);
+            avg_data = mean(pos_data,1);
+            ste_data = std(pos_data,1)/sqrt(size(pos_data,1));
+
+            data_matrix(pos,:) = avg_data;
+            ste_matrix(pos,:) = ste_data;      
+
+        end
+        
+        errorbar(line_x_data{stim_type}, data_matrix(:,muscle), ste_matrix(:,muscle), 'LineWidth', 2)
+
+    end
+    plot([1.5 1.5], [-10 10], 'k--', 'LineWidth', 2)
+    plot([2.5 2.5], [-10 10], 'k--', 'LineWidth', 2)
+    
+    
+    xticks([1 2 3])
+    xticklabels({'C3', 'C5', 'C7'})
+    title(muscle_title{muscle})
+    if muscle == 2
+        legend(field_name_list{stim_idx_list})
+    end
+    xlim([0.5 3.5])
+    ylim([0.1 0.4])
+    ylabel('Selectivity Index')
+    ax = gca;
+    ax.FontSize = 14;
+    
+    count = count+1;
+end
+
+
